a7070ae53e8c305ebdc7edb1d09d632831e9c3d9
Administrator <Administrator@SKY-20180503SJW>
2018-06-03 14:21:31 +0800

    updated by Zoey

---
+++ src/test/java/com/zoey/LessionFiveTest/LessionFiveDemo.java
@@ -0,0 +1,12 @@
+package com.zoey.LessionFiveTest;
+
+import org.testng.annotations.Test;
+
+public class LessionFiveDemo {
+	@Test
+	public void zoeyTest() {
+		System.out.println("zoey testing");
+		
+	}
+
+}
